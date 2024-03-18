e620262cf886cc671b0972b1df3ed02d7e993027
ANTONIO FRANCISCO ROMERO NAVARRO <afromero@alu.ucam.edu>
2024-03-18 10:10:05 +0100

    Explicacion de constantes correspondientes realizada

--- SistemaGestionEmpleados.java
+++ SistemaGestionEmpleados.java
@@ -1,39 +1,24 @@
 import java.util.Scanner;
 
 public class SistemaGestionEmpleados {
-
-    private Empleado[] empleados;
-
-    public SistemaGestionEmpleados(Empleado[] empleados) {
-        this.empleados = empleados;
-    }
-
-    public void aumentarSalario(double porcentaje) {
-        for (Empleado empleado : empleados) {
-            double nuevoSalario = empleado.salario * (1 + porcentaje / 100);
-            empleado.salario = nuevoSalario;
-        }
-    }
-
     public static void main(String[] args) {
         Empleado[] empleados = new Empleado[3];
-        empleados[0] = new Empleado("Juan", "Desarrollador", 50000);
-        empleados[1] = new Empleado("María", "Diseñadora", 45000);
-        empleados[2] = new Empleado("Pedro", "Gerente", 60000);
+        empleados[0] = new Empleado("Juan", Constantes.CARGO_DESARROLLADOR, 50000);
+        empleados[1] = new Empleado("María", Constantes.CARGO_DISEÑADOR, 45000);
+        empleados[2] = new Empleado("Pedro", Constantes.CARGO_GERENTE, 60000);
 
-        SistemaGestionEmpleados sistema = new SistemaGestionEmpleados(empleados);
+        Empleados gestorEmpleados = new Empleados(empleados);
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Introduzca el porcentaje de aumento de salario: ");
+        System.out.print(Constantes.MENSAJE_AUMENTO_SALARIO);
         double porcentaje = scanner.nextDouble();
 
-        sistema.aumentarSalario(porcentaje);
-        
-        System.out.println("Lista de Empleados:");
-        for (Empleado empleado : empleados) {
-            System.out.println(empleado);
-        }
+        gestorEmpleados.aumentarSalario(porcentaje);
+
+        System.out.println(Constantes.MENSAJE_LISTA_EMPLEADOS);
+        gestorEmpleados.mostrarListado();
 
         scanner.close();
     }
 }
+
